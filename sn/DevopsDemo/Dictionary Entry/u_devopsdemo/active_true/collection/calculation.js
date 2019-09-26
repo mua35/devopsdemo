b27374e8c3e5766db652f4abb8784e1497e07314
System Administrator <admin@example.com>
2019-09-26 12:59:42 +0530

    Changes by 'System Administrator' on 'DevopsDemo – 2.0.0' -- Build #1

---
+++ sn/DevopsDemo/Dictionary Entry/u_devopsdemo/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : DevopsDemo
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-26 07:22:11
+ * Created By  : admin
+ * Updated On  : 2019-09-26 07:22:11
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=2fc46dfe1b888c109614ff37dc4bcb88
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
